cdc31ec1cb6712e0d4b5d939532ed57f3d2b8916
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-06-24 00:03:47 +0100

    Add some logic for auto-check.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,30 +1,36 @@
+-- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
 
+-- Create the Authors table
 CREATE TABLE IF NOT EXISTS Authors(
-  author_id INT PRIMARY KEY,
-  author_name VARCHAR(215)
+  author_id INT AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(215) NOT NULL
 );
 
+-- Create the Books table
 CREATE TABLE IF NOT EXISTS Books(
-  book_id INT PRIMARY KEY,
+  book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130),
   author_id INT,
-  price DOUBLE,
+  price DOUBLE NOT NULL,
   publication_date DATE,
 
-  FOREIGN KEY(author_id)
+  FOREIGN KEY (author_id)
   REFERENCES Authors(author_id)
 );
 
+-- Create the Customers table
 CREATE TABLE IF NOT EXISTS Customers(
-  customer_id INT PRIMARY KEY,
-  customer_name VARCHAR(215),
-  email VARCHAR(215),
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) NOT NULL UNIQUE,
   address TEXT
 );
 
+-- Create the Orders table
 CREATE TABLE IF NOT EXISTS Orders(
-  order_id INT PRIMARY KEY,
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_date DATE,
 
@@ -32,22 +38,23 @@ CREATE TABLE IF NOT EXISTS Orders(
   REFERENCES Customers(customer_id)
 );
 
+-- Create the Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details(
-  orderdetailid INT PRIMARY KEY,
+  orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
   order_id INT,
   book_id INT,
-  quantity DOUBLE,
+  quantity DOUBLE NOT NULL,
 
-  CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (order_id)
+  FOREIGN KEY (order_id)
   REFERENCES Orders(order_id),
 
-  CONSTRAINT FK_OrderDetails_Books FOREIGN KEY (book_id)
+  FOREIGN KEY (book_id)
   REFERENCES Books(book_id)
 );
 
-SHOW TABLES;
-SELECT * FROM Authors;
-SELECT * FROM Books;
-SELECT * FROM Customers;
-SELECT * FROM Orders;
-SELECT * FROM Order_Details;
+-- SHOW TABLES;
+-- SELECT * FROM Authors;
+-- SELECT * FROM Books;
+-- SELECT * FROM Customers;
+-- SELECT * FROM Orders;
+-- SELECT * FROM Order_Details;
