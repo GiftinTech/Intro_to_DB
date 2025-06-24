c04786f3b6511d17e98bffdeedc88409e8ec1371
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-06-24 01:14:54 +0100

    Change casing for autocheck.

--- task_2.sql
+++ task_2.sql
@@ -1,11 +1,11 @@
--- Create Authors table
-CREATE TABLE IF NOT EXISTS authors(
+-- Create the Authors table
+CREATE TABLE IF NOT EXISTS Authors(
   author_id INT PRIMARY KEY AUTO_INCREMENT,
   author_name VARCHAR(215) NOT NULL
 );
 
--- Create Books table
-CREATE TABLE IF NOT EXISTS books(
+-- Create the Books table
+CREATE TABLE IF NOT EXISTS Books(
   book_id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(130) NOT NULL,
   author_id INT NOT NULL,
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS books(
   publication_date DATE,
 
   CONSTRAINT FK_AuthorID FOREIGN KEY (author_id)
-  REFERENCES authors(author_id)
+  REFERENCES Authors(author_id)
 );
 
 -- Create the Customers table
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE IF NOT EXISTS Customers(
   customer_id INT PRIMARY KEY AUTO_INCREMENT,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215) NOT NULL UNIQUE,
@@ -25,26 +25,26 @@ CREATE TABLE IF NOT EXISTS customers(
 );
 
 -- Create the Orders table
-CREATE TABLE IF NOT EXISTS orders(
+CREATE TABLE IF NOT EXISTS Orders(
   order_id INT PRIMARY KEY AUTO_INCREMENT,
   customer_id INT NOT NULL,
   order_date DATE,
-  CONSTRAINT FK_CustomerId FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+  CONSTRAINT FK_CustomerId FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table
-CREATE TABLE IF NOT EXISTS order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
   orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
   order_id INT NOT NULL,
   book_id INT NOT NULL,
   quantity DOUBLE NOT NULL,
-  CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  CONSTRAINT FK_OrderDetails_Books FOREIGN KEY (book_id) REFERENCES books(book_id)
+  CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  CONSTRAINT FK_OrderDetails_Books FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
-SHOW TABLES;
-SELECT * FROM authors;
-SELECT * FROM books;
-SELECT * FROM customers;
-SELECT * FROM orders;
-SELECT * FROM order_details;
+-- SHOW TABLES;
+-- SELECT * FROM Authors;
+-- SELECT * FROM Books;
+-- SELECT * FROM Customers;
+-- SELECT * FROM Orders;
+-- SELECT * FROM Order_Details;
